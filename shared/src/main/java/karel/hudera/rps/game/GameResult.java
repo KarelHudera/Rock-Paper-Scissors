1e2772f8d187ae1d9260abe079e5d662877fda0f
Scar <karel.hudera@gmail.com>
2025-02-23 10:37:19 +0100

    feat: server setup

---
+++ shared/src/main/java/karel/hudera/rps/game/GameResult.java
@@ -0,0 +1,24 @@
+package karel.hudera.rps.game;
+
+import java.io.Serializable;
+
+public class GameResult implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String player1;
+    private String move1;
+    private String player2;
+    private String move2;
+    private String result;
+
+    public GameResult(String player1, String move1, String player2, String move2, String result) {
+        this.player1 = player1;
+        this.move1 = move1;
+        this.player2 = player2;
+        this.move2 = move2;
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
