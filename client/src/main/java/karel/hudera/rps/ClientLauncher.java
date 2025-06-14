b79aa5881aebaa454f2e25da67a5500344b4483b
adela-domokosova <adela.domokosova@gmail.com>
2025-06-14 23:49:49 +0200

    feat: plugins for fat jar

---
+++ client/src/main/java/karel/hudera/rps/ClientLauncher.java
@@ -0,0 +1,9 @@
+package karel.hudera.rps;
+
+import javafx.application.Application;
+
+public class ClientLauncher {
+    public static void main(String[] args) {
+        Application.launch(StartClient.class, args);
+    }
+}
